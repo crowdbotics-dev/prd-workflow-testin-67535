6d32ca00dfaa4cafce67a6137740726ec83c0964
Crowdbotics <engineering@crowdbotics.com>
2023-03-02 19:00:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "prd-workflow-testin-67535",
+  url: "https://prd-workflow-testin-67535.botics.co",
+  api: "https://prd-workflow-testin-67535.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
